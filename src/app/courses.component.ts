ce3c8114bda86ac52eee636805f54c290987d52e
Deepa <deepa@Holitions-iMac-3.local>
2019-11-29 16:17:49 +0000

    displayed a list in the bowser using previous service and constructor

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,23 +3,34 @@ import {Component} from "@angular/core"
 import {CoursesService} from "./courses.service";
 @Component({//Html mark up to render this app
     selector:'courses',
-    template:`<h2>Courses</h2> {{title}}
-    <ul >
-    <li *ngFor="let course of courses">{{course}}</li> 
-    </ul>`
+    template:`
+    <h2>Courses</h2>
+    {{title}}
+    <ul>
+        <li *ngFor="let course of courses">
+            {{course}}
+        </li> 
+        
+    </ul>
+    <ul>
+       <li *ngFor="let author of authors">
+            {{author}}
+       </li> 
+        
+    </ul>
+`
 })
+
 
 export class CoursesComponent{
     title="list of courses here";
     courses;
+    authors;
 
     constructor(service:CoursesService){
         this.courses=service.getCourses();
-
-
+        this.authors=service.getAuthors();
     }
-    
-
 }
 //2.Register this component in an module.Here currently one module its app (->app.module.ts)
 //3. selector render the component in (component.html)
--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -1,4 +1,9 @@
 export class CoursesService{
-    getCourses(){
-    return["course1","course2","course3"];
-    }}
+    getCourses(): string[] {
+        return["course1","course2","course3"];
+    }
+    getAuthors():string[]{
+        return["author1","author2","author3"]
+    }
+
+}
